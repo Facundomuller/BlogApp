47b9d45749d656fb4117870cdabe4a4cf4090fd5
facundokudos <facundo@kudosestudio.com>
2021-04-10 18:14:16 -0300

    Project Starts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Hola from './home/hola';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Hola firstname="Facundo" secondname="Müller"></Hola>
+        <h2>BlogApp</h2>
+        <p>La nueva forma de hacer blogs</p>
       </header>
     </div>
   );
---
+++ src/home/hola.js
@@ -0,0 +1,10 @@
+function Hola(props) {
+    return(
+        <div>
+            <h3>¡Bienvenido {props.firstname} {props.secondname}!</h3>
+            <p>Te presentamos:</p>
+        </div>
+    );
+}
+
+export default Hola
